5c55f7659f764f5b31eb0ef15cf50a61047e0a51
CADatt <89214415+CADatt@users.noreply.github.com>
2022-02-20 20:00:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,102 @@
+var starimg,star,score;
+var rocketimg,rocket;
+var spaceimg,space;
+var obstacle,obstacleimg,obstaclegroup;
+var PLAY=1;
+var END=0;
+var gamestate=PLAY;
+
+function preload(){
+  starimg=loadImage("star.png")
+  rocketimg=loadImage("rocket.png");
+  spaceimg=loadImage("space.jpg"); 
+  obstacleimg=loadImage("meteor.png"); 
+
+}
+
+function setup() {
+    createCanvas(600,600);
+
+    space=createSprite(300,300,100,100);
+    space.addImage("space",spaceimg);
+    space.velocity.y=1;
+    
+    rocket= createSprite(400,300,1,1);
+    rocket.addImage("rocket",rocketimg);
+    rocket.scale=0.025;
+    obstaclegroup =new Group();
+    score=0;
+}
+
+function draw() {
+    background(200);
+    text("score:"+score,500,50);
+    if (gamestate===PLAY) {
+        if (keyDown("left_arrow")) {
+            rocket.x=rocket.x-3;
+        }
+        if (keyDown("right_arrow")) {
+            rocket.x=rocket.x+3;
+        }
+        //if (keyDown("up_arrow")) {
+           // rocket.y=rocket.y-3;
+        //}
+        if (keyDown("down_arrow")) {
+            rocket.y=rocket.y+3;
+        }
+        if(space.y > 325){
+            space.y = 300;
+        }
+        rocket.velocityY+=0.5;
+        if (keyDown("space")) {
+            rocket.velocityY=-5;
+        }
+        
+        spawnobstacles();
+        spawnstar();
+        
+        if (rocket.isTouching(obstaclegroup)) {
+          rocket.velocityY=0;
+          gamestate=END;
+         }
+        if (rocket.isTouching(star)) {
+            score=score+1; 
+         } 
+        
+
+
+    }
+
+    if (gamestate===END) {
+        space.destroy();
+        rocket.destroy();
+        obstaclegroup.destroyEach();
+        star.destroy();
+        text("GAME OVER",300,300);
+    }
+    
+ drawSprites();
+ 
+}
+
+function spawnobstacles() {
+    if (frameCount%100===0){
+    obstacle= createSprite(Math.round(random(150,450)),150,10,10);
+    obstacle.addImage("obstacle",obstacleimg);
+    obstacle.scale=0.1;
+    obstacle.lifetime=600;  
+    obstacle.velocityY=5;
+    obstaclegroup.add(obstacle);   
+
+    }
+}
+
+function spawnstar() {
+    if (frameCount%200===0) {
+        star = createSprite(Math.round(random(50,450)),50,10,10);
+        star.addImage("star",starimg);
+        star.scale=0.1;
+        star.lifetime=600;
+        star.velocityY=5;
+    }
+}
